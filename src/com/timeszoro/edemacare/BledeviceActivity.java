1433eb6f470d0ac0317d338e8f81eb7e8e51deae
Timeszoro <wgjing307@yeah.net>
2014-12-22 16:00:13 +0800

    tmp

--- src/com/timeszoro/edemacare/BledeviceActivity.java
+++ src/com/timeszoro/edemacare/BledeviceActivity.java
@@ -2,11 +2,12 @@ package com.timeszoro.edemacare;
 
 
 
-import android.app.Activity;
+import java.lang.reflect.Field;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.ViewConfiguration;
+
 import com.example.edemacare.R;
 
 
@@ -17,11 +18,35 @@ public class BledeviceActivity extends FragmentActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bledevice);
-		
+		forceShowOverflowMenu();
 		getSupportFragmentManager().beginTransaction().add(R.id.fragment_blelist, new BleListFragment()).commit();
 		getSupportFragmentManager().beginTransaction().add(R.id.fragment_scanbtn, new ScanProFragment()).commit();
 		
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.bledevice, menu);
+		return true;
+	}
+	/**
+	 * show the overflow button
+	 */
+    private void forceShowOverflowMenu() {  
+        try {  
+            ViewConfiguration config = ViewConfiguration.get(this);  
+            Field menuKeyField = ViewConfiguration.class  
+                    .getDeclaredField("sHasPermanentMenuKey");  
+            if (menuKeyField != null) {  
+                menuKeyField.setAccessible(true);  
+                menuKeyField.setBoolean(config, false);  
+            }  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+    
+    
 //	@Override
 //	protected Fragment createFragment() {
 //		return new ScanProFragment();
